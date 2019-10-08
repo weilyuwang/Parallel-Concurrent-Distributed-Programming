9f6e53d5f1e5fed8a685fdf2cd9a45845fe9281f
Weilyu Wang <weilyuwang@gmail.com>
2019-10-08 16:44:27 -0400

    completed minproject 1

--- Distributed-Programming/miniproject_1/src/main/java/edu/coursera/distributed/PageRank.java
+++ Distributed-Programming/miniproject_1/src/main/java/edu/coursera/distributed/PageRank.java
@@ -4,6 +4,10 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaPairRDD;
 import scala.Tuple2;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * A wrapper class for the implementation of a single iteration of the iterative
  * PageRank algorithm.
@@ -49,6 +53,21 @@ public final class PageRank {
     public static JavaPairRDD<Integer, Double> sparkPageRank(
             final JavaPairRDD<Integer, Website> sites,
             final JavaPairRDD<Integer, Double> ranks) {
-        throw new UnsupportedOperationException();
+        return sites.join(ranks)
+                .flatMapToPair(kv -> {
+                    Website edges = kv._2()._1();
+                    Double currentRank = kv._2()._2();
+
+                    List<Tuple2<Integer, Double>> contribs = new LinkedList<>();
+                    Iterator<Integer> iter = edges.edgeIterator();
+
+                    while(iter.hasNext()) {
+                        final int target = iter.next();
+                        contribs.add(new Tuple2<>(target, currentRank / (double) edges.getNEdges()));
+                    }
+                    return contribs;
+                })
+                .reduceByKey((Double d1, Double d2) -> d1 + d2)
+                .mapValues(v -> 0.15 + 0.85 * v);
     }
 }
